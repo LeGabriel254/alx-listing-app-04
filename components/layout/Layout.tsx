99004beec6a34cc2d02437ee5228fc9eccf7f288
Leon Gabriel <g73346920@gmail.com>
2025-02-17 22:25:40 +0300

    Initial commits

---
+++ components/layout/Layout.tsx
@@ -0,0 +1,15 @@
+import Header from "../layout/Header";
+import Footer from "../layout/Footer";
+import { ComponentProps } from "../../interfaces";
+
+const Layout: React.FC<ComponentProps> = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main className="min-h-screen">{children}</main>
+      <Footer />
+    </>
+  );
+}
+
+export default Layout;
